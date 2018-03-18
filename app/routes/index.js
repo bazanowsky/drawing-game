939ea604d00e3981d503686db90b92b19859d9c9
Marcin Bazanowski <mbazanowski@apptension.com>
2018-03-18 15:15:42 +0100

    add atlaskit reset + base styles

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 
+/* eslint-disable-next-line */
+import '@atlaskit/css-reset';
+
+import '../theme/global';
+
 import App from './app.container';
 import { Home } from './home';
 import { NotFound } from './notFound';
