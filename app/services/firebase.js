1842faa48b5085ec102c6b3a0edc563dc67d03e3
Marcin Bazanowski <mbazanowski@apptension.com>
2018-03-18 16:05:36 +0100

    configure firebase

---
+++ app/services/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyD_J2yMP23-xuMVEvqIRGk7ZflnutRKiE8',
+  authDomain: 'what-am-i-drawing.firebaseapp.com',
+  databaseURL: 'https://what-am-i-drawing.firebaseio.com',
+  projectId: 'what-am-i-drawing',
+  storageBucket: 'what-am-i-drawing.appspot.com',
+  messagingSenderId: '296190757104',
+};
+firebase.initializeApp(config);
+
+export const database = firebase.database();
+
+export default firebase;
